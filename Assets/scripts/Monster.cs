440d5f85585a89b5eb2e3192df206d2a1d46959e
José Calderón <jgcalderon372@gmail.com>
2024-04-17 13:10:23 -0500

    Se agregan comentarios explicativos del código

--- Assets/scripts/Monster.cs
+++ Assets/scripts/Monster.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
+// Definición de la clase Spell, que representa un hechizo con nombre, daño y dificultad para acertar.
 [System.Serializable]
 public class Spell
 {
@@ -9,6 +10,8 @@ public class Spell
     public int damage;
     public int difficultyToHit;
 }
+
+// Definición de la clase MonsterZone, que representa una zona de monstruos con nombre, salud máxima, hechizos, imagen y si es un jefe o no.
 [System.Serializable]
 public class MonsterZone
 {
@@ -21,82 +24,94 @@ public class MonsterZone
 
     public bool isBoss;
     
-    public string zone = "Tainela"; // Tainela en este caso en esta primer version solo tendrá esta zona
+    public string zone = "Tainela"; // Tainela es la única zona disponible en esta versión.
 
 }
 
+// Clase principal Monster que controla el comportamiento de los monstruos en el juego.
 public class Monster : MonoBehaviour
 {
-    public MonsterZone[] monsterZones;
-    public MonsterZone currentMonsterZone;
+    public MonsterZone[] monsterZones; // Array de zonas de monstruos disponibles.
+    public MonsterZone currentMonsterZone; // Zona de monstruo actual en la que se encuentra el monstruo.
+    public Slider estadoVidaMonstruo; // Referencia al componente Slider que muestra la vida del monstruo en la UI.
 
-    public Image monsterImageUI; // Referencia al componente Image en la UI
-    public Player player;
-    private UIManager uiManager;
-    public bool hasAttacked = false;
+    public Image monsterImageUI; // Referencia al componente Image en la UI para mostrar la imagen del monstruo.
+    public Player player; // Referencia al jugador.
+    private UIManager uiManager; // Referencia al UIManager para controlar elementos de la UI.
+    public bool hasAttacked = false; // Indica si el monstruo ya ha atacado.
     
-    // Start is called before the first frame update
+    // Start se llama antes de la primera actualización del frame.
     void Start()
     {
-        uiManager = FindObjectOfType<UIManager>();
-        SelectRandomMonsterZone();
-        monsterImageUI.sprite = currentMonsterZone.monsterImage;
+        uiManager = FindObjectOfType<UIManager>(); // Busca el UIManager en la escena.
+        estadoVidaMonstruo = FindObjectOfType<Slider>(); // Busca el componente Slider en la escena.
+        SelectRandomMonsterZone(); // Selecciona una zona de monstruo al azar al iniciar.
+        monsterImageUI.sprite = currentMonsterZone.monsterImage; // Establece la imagen del monstruo en la UI.
     }
 
+    // Selecciona una zona de monstruo al azar y establece la salud actual igual a la salud máxima.
     void SelectRandomMonsterZone()
     {
-        int index = Random.Range(0, monsterZones.Length);
-        currentMonsterZone = monsterZones[index];
-        currentMonsterZone.actualHealth = currentMonsterZone.maxHealth;
+        int index = Random.Range(0, monsterZones.Length); // Selecciona un índice al azar.
+        currentMonsterZone = monsterZones[index]; // Establece la zona de monstruo actual.
+        currentMonsterZone.actualHealth = currentMonsterZone.maxHealth; // Establece la salud actual a la máxima.
+        estadoVidaMonstruo.minValue = 0; // Establece el valor mínimo del Slider de vida.
+        estadoVidaMonstruo.maxValue = currentMonsterZone.maxHealth; // Establece el valor máximo del Slider de vida.
+        estadoVidaMonstruo.value = currentMonsterZone.maxHealth; // Establece el valor actual del Slider de vida.
     }
 
+    // Intenta realizar un ataque.
     public void TryAttack()
     {
-        if (hasAttacked) return;
+        if (hasAttacked) return; // Si ya ha atacado, no hace nada.
         
-        int resultadoDado = GameManager.Instance.rollDieMonster();
-        Spell hechizoSeleccionado = SelectRandomSpell();
+        int resultadoDado = GameManager.Instance.rollDieMonster(); // Lanza un dado para el monstruo.
+        Spell hechizoSeleccionado = SelectRandomSpell(); // Selecciona un hechizo al azar.
 
-        StartCoroutine(ManejarAtaque(resultadoDado, hechizoSeleccionado));
+        StartCoroutine(ManejarAtaque(resultadoDado, hechizoSeleccionado)); // Inicia la corutina para manejar el ataque.
     }
 
+    // Corutina que maneja el ataque.
     IEnumerator ManejarAtaque(int resultadoDado, Spell hechizoSeleccionado)
     {
-        yield return new WaitForSeconds(5); 
+        yield return new WaitForSeconds(5); // Espera 5 segundos.
         
+        // Si el resultado del dado es mayor o igual a la dificultad para acertar, no es el turno del jugador y el estado de la partida es 1 (combate),
+        // entonces el jugador pierde vida y se muestra un mensaje.
         if (resultadoDado >= hechizoSeleccionado.difficultyToHit 
             && GameManager.Instance.turnoPlayer == false 
             && GameManager.Instance.estadoPartida == 1)
         {
-            player.vida -= hechizoSeleccionado.damage;
+            player.vida -= hechizoSeleccionado.damage; // El jugador pierde vida.
             uiManager.ShowMessage(currentMonsterZone.name + " te ataca con " + 
                                   hechizoSeleccionado.name + " pierdes " +
-                                  hechizoSeleccionado.damage + " de vida");
-            uiManager.txtVida.text = "Vida  " + player.vida;
-            StartCoroutine(uiManager.Wait(5));
-            GameManager.Instance.turnosCombate += 1;
-            GameManager.Instance.turnoPlayer = true;
+                                  hechizoSeleccionado.damage + " de vida"); // Muestra mensaje de ataque.
+            uiManager.txtVida.text = "Vida  " + player.vida; // Actualiza la vida del jugador en la UI.
+            StartCoroutine(uiManager.Wait(5)); // Espera 5 segundos.
+            GameManager.Instance.turnosCombate += 1; // Incrementa el contador de turnos de combate.
+            GameManager.Instance.turnoPlayer = true; // Cambia el turno al jugador.
         }
         else
         {
-            uiManager.ShowMessage(currentMonsterZone.name + " ha fallado su ataque.");
-            StartCoroutine(uiManager.Wait(5));
-            GameManager.Instance.turnosCombate += 1;
-            GameManager.Instance.turnoPlayer = true;
+            uiManager.ShowMessage(currentMonsterZone.name + " ha fallado su ataque."); // Muestra mensaje de ataque fallido.
+            StartCoroutine(uiManager.Wait(5)); // Espera 5 segundos.
+            GameManager.Instance.turnosCombate += 1; // Incrementa el contador de turnos de combate.
+            GameManager.Instance.turnoPlayer = true; // Cambia el turno al jugador.
         }
-        hasAttacked = true;
+        hasAttacked = true; // Indica que el monstruo ya ha atacado.
     }
 
+    // Selecciona un hechizo al azar de la zona de monstruo actual.
     Spell SelectRandomSpell()
     {
-        int index = Random.Range(0, currentMonsterZone.spells.Length);
-        return currentMonsterZone.spells[index];
+        int index = Random.Range(0, currentMonsterZone.spells.Length); // Selecciona un índice al azar.
+        return currentMonsterZone.spells[index]; // Devuelve el hechizo seleccionado.
     }
     
-    // Update is called once per frame
+    // Update se llama una vez por frame.
     void Update()
     {
-        Debug.Log("VIDA MONSTRUO: " + currentMonsterZone.actualHealth);
-        Debug.Log(hasAttacked ? "YA ATACO EL MONSTRUO": "NO HA ATACADO EL MONSTRUO");
+        Debug.Log("VIDA MONSTRUO: " + currentMonsterZone.actualHealth); // Muestra la vida actual del monstruo.
+        Debug.Log(hasAttacked ? "YA ATACO EL MONSTRUO": "NO HA ATACADO EL MONSTRUO"); // Indica si el monstruo ha atacado o no.
     }
 }
--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using UnityEngine;
 
+// Se define una clase serializable para representar los hechizos del jugador, con nombre, daño y dificultad para acertar.
 [System.Serializable]
 public class PlayerSpell
 {
@@ -10,53 +11,64 @@ public class PlayerSpell
     public int difficultyToHit;
 }
 
+// Clase principal del jugador que hereda de MonoBehaviour para poder interactuar con Unity.
 public class Player : MonoBehaviour
 {
+    // Variables para la vida, kamas (moneda del juego) y nivel del jugador, inicializadas.
     public int vida = 10;
     public int kamas = 0;
     public int nivel = 1;
+    // Array de ataques disponibles para el jugador.
     public PlayerSpell[] ataques;
+    // Referencia privada al UIManager para interactuar con la UI.
     private UIManager _uiManager;
 
-    // Start is called before the first frame update
+    // Start se llama antes de la primera actualización del frame, inicializa el UIManager.
     void Start()
     {
         _uiManager = FindObjectOfType<UIManager>();
     }
 
+    // Método para realizar un ataque, recibe el índice del ataque a utilizar.
     public void Ataque(int index)
     {
+        // Verifica si el índice es válido, si no, muestra un error y sale del método.
         if (index < 0 || index >= ataques.Length)
         {
             Debug.LogError("Índice de ataque no válido.");
             return;
         }
 
+        // Lanza un dado para determinar el resultado del ataque y selecciona el hechizo basado en el índice.
         int resultadoDado = GameManager.Instance.rollDiePlayer();
         PlayerSpell hechizoSeleccionado = ataques[index];
+        // Encuentra al monstruo en la escena para interactuar con él.
         Monster monster = GameObject.Find("Monster").GetComponent<Monster>();
+        // Calcula si el ataque es crítico basado en el resultado del dado.
         int danioCritico = resultadoDado == 20 ? 1 : 0;
+        // Determina si el ataque acierta basado en la dificultad del hechizo y el resultado del dado.
         bool isHit = resultadoDado >= hechizoSeleccionado.difficultyToHit;
 
+        // Si el ataque acierta, es el turno del jugador, el estado de la partida es de combate y quedan turnos de combate...
         if (isHit && GameManager.Instance.turnoPlayer && 
             GameManager.Instance.estadoPartida == 2 && // COMBATE
             GameManager.Instance.turnosCombate > 0)
         {
+            // Se reduce la salud del monstruo y se verifica si ha sido derrotado.
             monster.currentMonsterZone.actualHealth -= (hechizoSeleccionado.damage + danioCritico);
             if (monster.currentMonsterZone.actualHealth <= 0)
             {
+                // Si el monstruo es derrotado, se cambia el estado de la partida, se desactiva el monstruo y se muestra un mensaje.
                 GameManager.Instance.estadoPartida = 1;
                 monster.gameObject.SetActive(false);
-                _uiManager.ShowMessage($"Has derrotado al {monster.currentMonsterZone.name}");
-                /*StartCoroutine(_uiManager.Wait(5));
-                Debug.Log("MONSTRUO DERROTADO");
-                GameManager.Instance.turnosCombate = 0;
-                monster.hasAttacked = true;
-                GameManager.Instance.turnoPlayer = true;*/
+                _uiManager.ShowMessage($"Has derrotado al {monster.currentMonsterZone.name}\n has ganado 1 KAMA");
+                this.kamas += 1;
+                _uiManager.ShowMessage("" + this.kamas);
                 StartCoroutine(WaitAndChangeState(5, monster));
             }
             else
             {
+                // Si el monstruo no es derrotado, se muestra un mensaje con el daño realizado.
                 string message = danioCritico != 0 ?
                     $"(CRÍTICO) Atacas al {monster.name} con {hechizoSeleccionado.name} " +
                     $"y pierde {hechizoSeleccionado.damage + danioCritico} de vida" :
@@ -69,15 +81,18 @@ public class Player : MonoBehaviour
         }
         else
         {
+            // Si el ataque falla, se muestra un mensaje y se cambia el turno.
             _uiManager.ShowMessage("Haz fallado tu ataque.");
             StartCoroutine(WaitAndChangeTurn(5, false));
         }
+        // Espera antes de continuar con el siguiente turno.
         StartCoroutine(_uiManager.Wait(5));
         monster.hasAttacked = false;
         GameManager.Instance.turnoPlayer = false;
         GameManager.Instance.turnosCombate += 1;
     }
     
+    // Corutinas para manejar el cambio de estado y turno después de un delay.
     private IEnumerator WaitAndChangeState(float delay, Monster monster)
     {
         yield return new WaitForSeconds(delay);
@@ -93,11 +108,12 @@ public class Player : MonoBehaviour
     }
     
 
-    // Métodos de ataque individuales que llaman al método general Ataque
+    // Métodos de ataque individuales que llaman al método general Ataque con diferentes índices.
     public void Ataque1() { Ataque(0); }
     public void Ataque2() { Ataque(1); }
     public void Ataque3() { Ataque(2); }
 
+    // Update se llama una vez por frame, muestra la vida del jugador en consola.
     private void Update()
     {
         Debug.Log("VIDA JUGADOR: " + vida);
